b77c86e9a89a432e8b67cd31732f7f0758ecd52b
Scott Offen <github@scottoffen.com>
2019-02-27 09:49:47 -0700

    Useful for showing all of the IPv4 addresses on a computer

---
+++ src/Samples.Objects/DisplayIpAddresses.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Samples.Objects
+{
+    public static class DisplayIPAddresses
+    {
+
+        public static void Run()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            // Get a list of all network interfaces (usually one per network card, dialup, and VPN connection)
+            NetworkInterface[] networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (NetworkInterface network in networkInterfaces)
+            {
+                // Read the IP configuration for each network
+                IPInterfaceProperties properties = network.GetIPProperties();
+
+                // Each network interface may have multiple IP addresses
+                foreach (IPAddressInformation address in properties.UnicastAddresses)
+                {
+                    // We're only interested in IPv4 addresses for now
+                    if (address.Address.AddressFamily != AddressFamily.InterNetwork)
+                        continue;
+
+                    // Ignore loopback addresses (e.g., 127.0.0.1)
+                    if (IPAddress.IsLoopback(address.Address))
+                        continue;
+
+                    sb.AppendLine(address.Address.ToString() + " (" + network.Name + ")");
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+            // MessageBox.Show(sb.ToString());
+        }
+    }
+}
